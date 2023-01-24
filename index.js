0c60b8c9795d7c8488d182e4cdedf485eafd86c0
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2023-01-23 20:35:30 -0400

    mais uma

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const sonic = document.querySelector('.sonic');
 const enemy = document.querySelector('.enemy');
+const cloud = document.querySelector('.clouds');
 
 const jump = () => {
     sonic.classList.add('jump');
     sonic.src = 'imagem/sonic-jump.GIF';
-    sonic.style.width = `100px`;
+    sonic.style.width = `90px`;
     setTimeout(() => {
         sonic.classList.remove('jump');
         sonic.src = 'imagem/sonic-run.GIF';
         sonic.style.width = '140px '
-    }, 500);
+    }, 600);
 }
 
 const loop = setInterval(() => {
 
     const enemyPosition = enemy.offsetLeft;
     const sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '');
+    const cloudPosition = cloud.offsetLeft;
 
     //console.log(sonicPosition);
 
@@ -24,8 +26,13 @@ const loop = setInterval(() => {
         enemy.style.left = `${enemyPosition}px`;
 
         sonic.style.animation = 'none';
-        setInterval(500 );
         sonic.style.bottom = `${sonicPosition}px`;
+        sonic.src = './imagem/sonic-game-over.png';
+        sonic.style.width = '170px';
+
+        cloud.style.left = `${cloudPosition}px`;
+        cloud.style.animation = 'none';
+
     }
 },10);
 
