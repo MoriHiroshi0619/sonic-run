d9630bf65a785c3cf8204fe3bab26791571978e0
MoriHiroshi0619 <mori.hiroshi.a.c@gmail.com>
2023-01-30 12:46:43 -0400

    aqui estou mais um dia
    tentando implementar score no jogo

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const sonic = document.querySelector('.sonic');
 const enemy = document.querySelector('.enemy');
 const cloud = document.querySelector('.clouds');
 
+var score = 0;
+var aux = true;
+
 const jump = () => {
     sonic.classList.add('jump');
     sonic.src = 'imagem/sonic-jump.GIF';
@@ -17,23 +20,45 @@ const loop = setInterval(() => {
 
     const enemyPosition = enemy.offsetLeft;
     const sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '');
+    const sonicLeftposition = +window.getComputedStyle(sonic).left.replace('px', '');
     const cloudPosition = cloud.offsetLeft;
 
-    //console.log(sonicPosition);
+    //console.log('loop'); 
+    // console.log(sonicLeftposition);
+
+    if(enemyPosition <= sonicLeftposition && aux == true){
+        score++; 
+        aux = false;
+    }
+
+    if(enemyPosition <= 0){
+        aux = true; 
+        console.log('passou o game board')
+    }
+
+    console.log(`score = ${score }`)
 
     if(enemyPosition <= 300 && enemyPosition >= 130 && sonicPosition <= 65){
-        enemy.style.animation = 'none';
+        enemy.style.animation = 'none'; 
         enemy.style.left = `${enemyPosition}px`;
-
+        
         sonic.style.animation = 'none';
-        sonic.style.bottom = `${sonicPosition}px`;
         sonic.src = './imagem/sonic-game-over.png';
         sonic.style.width = '170px';
-
+        sonic.style.bottom = `${sonicPosition}px`;
+        
         cloud.style.left = `${cloudPosition}px`;
         cloud.style.animation = 'none';
+         
 
+        clearInterval(loop); 
+        document.removeEventListener('keydown', jump);
+        
     }
 },10);
 
 document.addEventListener('keydown', jump);
+
+
+
+
